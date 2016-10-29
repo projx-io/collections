cd75ecf211d06685349733c9afb1faea3447ef08
Terrence Howard <chemisus@gmail.com>
2016-10-29 17:22:11 -0400

    FromOneTest

--- tests/unit/Common/FromOneTest.php
+++ tests/unit/Common/FromOneTest.php
@@ -1,79 +1,103 @@
 <?php
-//
-//namespace ProjxIO\Collections\Common;
-//
-//use ProjxIO\Collections\EntryItem;
-//use ProjxIO\Collections\TestCase;
-//
-//class FromOneTest extends TestCase
-//{
-//    /**
-//     * @dataProvider collectionProviderTest
-//     * @param FromOne $collection
-//     */
-//    public function testOffsetOfValue(FromOne $collection)
-//    {
-//        $this->assertEquals([0, 3], $collection->offsetOfValue('X'));
-//    }
-//
-//    /**
-//     * @dataProvider collectionProviderTest
-//     * @param FromOne $collection
-//     */
-//    public function testOffsetOfValues(FromOne $collection)
-//    {
-//        $this->assertEquals([[0, 3], [2, 5]], $collection->offsetOfValues(['X', 'Z']));
-//    }
-//
-//    /**
-//     * @dataProvider collectionProviderTest
-//     * @param FromOne $collection
-//     */
-//    public function testKeyOfValue(FromOne $collection)
-//    {
-//        $this->assertEquals(['A', 'D'], $collection->keyOfValue('X'));
-//    }
-//
-//    /**
-//     * @dataProvider collectionProviderTest
-//     * @param FromOne $collection
-//     */
-//    public function testKeyOfValues(FromOne $collection)
-//    {
-//        $this->assertEquals([['A', 'D'], ['C', 'B']], $collection->keyOfValues(['X', 'Z']));
-//    }
-//
-//    /**
-//     * @dataProvider collectionProviderTest
-//     * @param FromOne $collection
-//     */
-//    public function testItemOfValue(FromOne $collection)
-//    {
-//        $items = [
-//            new EntryItem('A', 'X'),
-//            new EntryItem('D', 'X'),
-//        ];
-//
-//        $this->assertItems($items, $collection->itemOfValue('X'));
-//    }
-//
-//    /**
-//     * @dataProvider collectionProviderTest
-//     * @param FromOne $collection
-//     */
-//    public function testItemOfValues(FromOne $collection)
-//    {
-//        $items = [
-//            [
-//                new EntryItem('A', 'X'),
-//                new EntryItem('D', 'X'),
-//            ],
-//            [
-//                new EntryItem('C', 'Z'),
-//                new EntryItem('B', 'Z'),
-//            ],
-//        ];
-//
-//        $this->assertItems($items, $collection->itemOfValues(['X', 'Z']));
-//    }
-//}
+
+namespace ProjxIO\Collections\Common;
+
+use ProjxIO\Collections\TestCase;
+
+/**
+ * @dataProvider
+ */
+class FromOneTest extends TestCase
+{
+    /**
+     * @dataProvider collectionProviderTest
+     * @param FromOne $collection
+     * @param mixed[] $v
+     * @param mixed[][] $ks
+     * @param mixed[] $k
+     * @param mixed[][] $vs
+     */
+    public function testOffsetOfValue(FromOne $collection, $v, $ks, $k, $vs)
+    {
+        $expect = array_keys($ks[$v[0]])[0];
+        $actual = $collection->offsetOfValue($v[0]);
+        $this->assertEquals($expect, $actual);
+    }
+
+    /**
+     * @dataProvider collectionProviderTest
+     * @param FromOne $collection
+     * @param mixed[] $v
+     * @param mixed[][] $ks
+     * @param mixed[] $k
+     * @param mixed[][] $vs
+     */
+    public function testOffsetOfValues(FromOne $collection, $v, $ks, $k, $vs)
+    {
+        $expect = [array_keys($ks[$v[0]])[0], array_keys($ks[$v[1]])[0]];
+        $actual = $collection->offsetOfValues([$v[0], $v[1]]);
+        $this->assertEquals($expect, $actual);
+    }
+
+    /**
+     * @dataProvider collectionProviderTest
+     * @param FromOne $collection
+     * @param mixed[] $v
+     * @param mixed[][] $ks
+     * @param mixed[] $k
+     * @param mixed[][] $vs
+     */
+    public function testKeyOfValue(FromOne $collection, $v, $ks, $k, $vs)
+    {
+        $expect = array_values($ks[$v[0]])[0];
+        $actual = $collection->keyOfValue($v[0]);
+        $this->assertEquals($expect, $actual);
+    }
+
+    /**
+     * @dataProvider collectionProviderTest
+     * @param FromOne $collection
+     * @param mixed[] $v
+     * @param mixed[][] $ks
+     * @param mixed[] $k
+     * @param mixed[][] $vs
+     */
+    public function testKeyOfValues(FromOne $collection, $v, $ks, $k, $vs)
+    {
+        $expect = [array_values($ks[$v[0]])[0], array_values($ks[$v[1]])[0]];
+        $actual = $collection->keyOfValues([$v[0], $v[1]]);
+        $this->assertEquals($expect, $actual);
+    }
+
+    /**
+     * @dataProvider collectionProviderTest
+     * @param FromOne $collection
+     * @param mixed[] $v
+     * @param mixed[][] $ks
+     * @param mixed[] $k
+     * @param mixed[][] $vs
+     */
+    public function testItemOfValue(FromOne $collection, $v, $ks, $k, $vs)
+    {
+        $keys = array_values($ks[$v[0]])[0];
+        $values = $v[0];
+        $actual = $collection->itemOfValue($v[0]);
+        $this->assertEntry($keys, $values, $actual);
+    }
+
+    /**
+     * @dataProvider collectionProviderTest
+     * @param FromOne $collection
+     * @param mixed[] $values
+     * @param mixed[][] $ks
+     * @param mixed[] $k
+     * @param mixed[][] $vs
+     */
+    public function testItemOfValues(FromOne $collection, $values, $ks, $k, $vs)
+    {
+        $keys = [array_values($ks[$values[0]])[0], array_values($ks[$values[1]])[0]];
+        $values = [$values[0], $values[1]];
+        $actual = $collection->itemOfValues([$values[0], $values[1]]);
+        $this->assertEntries($keys, $values, $actual);
+    }
+}
