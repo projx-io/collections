bca5fe8690244d6aea893604924818c6bf653d02
Terrence Howard <chemisus@gmail.com>
2016-10-30 16:50:42 -0400

    MutableFromToOneTest

--- tests/unit/Common/MutableFromToOneTest.php
+++ tests/unit/Common/MutableFromToOneTest.php
@@ -15,6 +15,38 @@ class MutableFromToOneTest extends TestCase
      * @param mixed[] $k
      * @param mixed[][] $vs
      */
+    public function testRemoveOffset(MutableFromToOne $collection, $v, $ks, $k, $vs)
+    {
+        $this->assertTrue($collection->containsEntry($k[0], $v[0]));
+        $collection->removeOffset(0);
+        $this->assertFalse($collection->containsEntry($k[0], $v[0]));
+    }
+
+    /**
+     * @dataProvider collectionProviderTest
+     * @param MutableFromToOne $collection
+     * @param mixed[] $v
+     * @param mixed[][] $ks
+     * @param mixed[] $k
+     * @param mixed[][] $vs
+     */
+    public function testRemoveOffsets(MutableFromToOne $collection, $v, $ks, $k, $vs)
+    {
+        $this->assertTrue($collection->containsEntry($k[0], $v[0]));
+        $this->assertTrue($collection->containsEntry($k[2], $v[2]));
+        $collection->removeOffsets([0, 2]);
+        $this->assertFalse($collection->containsEntry($k[0], $v[0]));
+        $this->assertFalse($collection->containsEntry($k[2], $v[2]));
+    }
+
+    /**
+     * @dataProvider collectionProviderTest
+     * @param MutableFromToOne $collection
+     * @param mixed[] $v
+     * @param mixed[][] $ks
+     * @param mixed[] $k
+     * @param mixed[][] $vs
+     */
     public function testRemoveEntry(MutableFromToOne $collection, $v, $ks, $k, $vs)
     {
         $this->assertTrue($collection->containsEntry($k[0], $v[0]));
@@ -55,5 +87,69 @@ class MutableFromToOneTest extends TestCase
         ]);
         $this->assertFalse($collection->containsEntry($k[0], $v[0]));
         $this->assertFalse($collection->containsEntry($k[2], $v[2]));
+    }
+
+    /**
+     * @dataProvider collectionProviderTest
+     * @param MutableFromToOne $collection
+     * @param mixed[] $v
+     * @param mixed[][] $ks
+     * @param mixed[] $k
+     * @param mixed[][] $vs
+     */
+    public function testRemoveValue(MutableFromToOne $collection, $v, $ks, $k, $vs)
+    {
+        $this->assertTrue($collection->containsValue($v[0]));
+        $collection->removeValue($v[0]);
+        $this->assertFalse($collection->containsValue($v[0]));
+    }
+
+    /**
+     * @dataProvider collectionProviderTest
+     * @param MutableFromToOne $collection
+     * @param mixed[] $v
+     * @param mixed[][] $ks
+     * @param mixed[] $k
+     * @param mixed[][] $vs
+     */
+    public function testRemoveValues(MutableFromToOne $collection, $v, $ks, $k, $vs)
+    {
+        $this->assertTrue($collection->containsValue($v[0]));
+        $this->assertTrue($collection->containsValue($v[2]));
+        $collection->removeValues([$v[0], $v[2]]);
+        $this->assertFalse($collection->containsValue($v[0]));
+        $this->assertFalse($collection->containsValue($v[2]));
+    }
+
+    /**
+     * @dataProvider collectionProviderTest
+     * @param MutableFromToOne $collection
+     * @param mixed[] $v
+     * @param mixed[][] $ks
+     * @param mixed[] $k
+     * @param mixed[][] $vs
+     */
+    public function testRemoveKey(MutableFromToOne $collection, $v, $ks, $k, $vs)
+    {
+        $this->assertTrue($collection->containsKey($k[0]));
+        $collection->removeKey($k[0]);
+        $this->assertFalse($collection->containsKey($k[0]));
+    }
+
+    /**
+     * @dataProvider collectionProviderTest
+     * @param MutableFromToOne $collection
+     * @param mixed[] $v
+     * @param mixed[][] $ks
+     * @param mixed[] $k
+     * @param mixed[][] $vs
+     */
+    public function testRemoveKeys(MutableFromToOne $collection, $v, $ks, $k, $vs)
+    {
+        $this->assertTrue($collection->containsKey($k[0]));
+        $this->assertTrue($collection->containsKey($k[2]));
+        $collection->removeKeys([$k[0], $k[2]]);
+        $this->assertFalse($collection->containsKey($k[0]));
+        $this->assertFalse($collection->containsKey($k[2]));
     }
 }
