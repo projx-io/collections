420d3e19161d32b1e6cfd19a4b0044c08794469c
Terrence Howard <chemisus@gmail.com>
2016-10-30 16:45:14 -0400

    removeItems

--- tests/unit/Common/MutableFromToOneTest.php
+++ tests/unit/Common/MutableFromToOneTest.php
@@ -36,4 +36,24 @@ class MutableFromToOneTest extends TestCase
         $collection->removeItem(new EntryItem($k[0], $v[0]));
         $this->assertFalse($collection->containsEntry($k[0], $v[0]));
     }
+
+    /**
+     * @dataProvider collectionProviderTest
+     * @param MutableFromToOne $collection
+     * @param mixed[] $v
+     * @param mixed[][] $ks
+     * @param mixed[] $k
+     * @param mixed[][] $vs
+     */
+    public function testRemoveItems(MutableFromToOne $collection, $v, $ks, $k, $vs)
+    {
+        $this->assertTrue($collection->containsEntry($k[0], $v[0]));
+        $this->assertTrue($collection->containsEntry($k[2], $v[2]));
+        $collection->removeItems([
+            new EntryItem($k[0], $v[0]),
+            new EntryItem($k[2], $v[2]),
+        ]);
+        $this->assertFalse($collection->containsEntry($k[0], $v[0]));
+        $this->assertFalse($collection->containsEntry($k[2], $v[2]));
+    }
 }
